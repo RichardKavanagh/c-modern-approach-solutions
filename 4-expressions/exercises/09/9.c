29b0121521e488aad8a010d4901f4c4647df6a5d
Richard Kavanagh <richard.kavanagh7@mail.dcu.ie>
2020-04-16 19:58:13 +0100

    Added solutions for exercise 9 and 10 of expressions

---
+++ 4-expressions/exercises/09/9.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+int main(void)
+{
+	
+	int i = 7;
+	int j = 8;
+	i *= j + 1;
+	printf("%d\n", i);
+	
+	int k;
+	i = j = k = 1;
+    i += j += k;
+    printf("%d %d %d\n", i, j, k);
+
+    i = 1; j = 2; k = 3;
+    i -= j -= k;
+    printf("%d %d %d\n", i, j, k);
+
+    i = 2; j = 1; k = 0;
+    i *= j *= k;
+    printf("%d %d %d\n", i, j, k);
+
+	return 0;
+}
