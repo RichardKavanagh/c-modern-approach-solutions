62547bdc393022ad9f42489e4608a38b77c6cb3e
Richard Kavanagh <richard.kavanagh7@mail.dcu.ie>
2020-04-27 21:10:21 +0100

    Added solution to exercise 3 of loops

---
+++ solutions/6-loops/projects/03/03.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main(void) {
+
+    int num, denom;
+    int n, m, temp;
+    
+    printf("%s", "Enter a fraction: ");
+    scanf("%d/%d", &num, &denom);
+
+    m = num;
+    n = denom;
+
+    do {
+        temp = m % n;
+        m = n;
+        n = temp;
+    } while (n != 0);
+
+    printf("%s%d/%d\n", "In lowest terms: ", num / m, denom / m);
+    return 0;
+}
