41a60e56e089103ba6d654e59e415ec8c065e6b0
Richard Kavanagh <richard.kavanagh7@mail.dcu.ie>
2020-05-24 00:32:26 +0100

    Added solution for project 6 of chapter Arrays

---
+++ solutions/8-arrays/projects/06/06.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define N 50
+
+int main(void) {
+
+    int len = 0;
+    char ch;
+    char input[N];
+    srand((unsigned) time(NULL));
+
+    printf("%s", "Enter message: ");
+    while((ch = getchar()) != '\n') {
+        input[len] = ch;
+        len++;
+    }
+
+    for (int i = 0; i < N; ++i) {
+        ch = input[i];
+        switch(ch) {
+            case 'a':
+                input[i] = '4';
+                break;
+            case 'b':
+                input[i] = '8';
+                break;
+            case 'e':
+                input[i] = '3';
+                break;
+            case 'i':
+                input[i] = '1';
+                break;
+            case 'o':
+                input[i] = '0';
+                break;
+            case 's':
+                input[i] = '5';
+                break;
+            default:
+                input[i] = toupper(ch);
+        }
+    }
+
+    /* generate a number between 8 and 12 */
+    int random_number = (rand() % 12) + 10;
+    for (int i = 1; i < random_number; ++i) {
+        input[len + i] = '!';
+    }
+
+    printf("%s", "In B1FF-speak: ");
+    for (int i = 0; i < len + random_number; ++i) {
+        printf("%c", input[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
