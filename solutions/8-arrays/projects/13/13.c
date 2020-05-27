a35fef7bc733f83cafc13b75dc01e5ac252d1c26
Richard Kavanagh <richard.kavanagh7@mail.dcu.ie>
2020-05-27 22:23:41 +0100

    Added solution for project 13 of arrays chapter

---
+++ solutions/8-arrays/projects/13/13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define N 10
+
+int main(void) {
+
+    int len = 0;
+    char ch, first_inital, last_name[N];
+    printf("%s", "Enter a first and last name: ");
+
+    first_inital = getchar();
+    while ((getchar()) != ' ') {
+        ;
+    }
+    
+    while ((ch = getchar()) != '\n') {
+        last_name[len] = ch;
+        len++;
+    }
+
+    printf("%s", "You entered the name: " );
+    for (int i = 0; i < len; ++i) {
+        putchar(last_name[i]);
+    }
+    printf("%s%c%s\n", ", ", first_inital, ".");
+
+    return 0;
+}
